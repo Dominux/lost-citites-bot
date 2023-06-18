f00a22bd8221dee5223db4681e4cefcdd06b505e
Dominux <kir.gimranov@gmail.com>
2023-06-18 21:33:14 +0300

    game_lib is done

--- game_lib/src/lib.rs
+++ game_lib/src/lib.rs
@@ -1,7 +1,27 @@
-mod common;
-mod config;
-mod entities;
-mod game;
+pub mod common;
+pub mod config;
+pub mod entities;
+pub mod game;
 
 #[cfg(test)]
 mod tests;
+
+/// Usage
+///
+/// ```
+/// use game_lib::config::Config;
+/// use game_lib::GameBuilder;
+///
+/// ...
+///
+/// let config = Config::new(...);
+/// let game = GameBuilder::build(config);
+/// ```
+pub struct GameBuilder;
+
+impl GameBuilder {
+    pub fn build(config: &config::Config) -> game::Game {
+        let shuffler = common::shuffler::RandomShuffler;
+        game::Game::new(config, shuffler)
+    }
+}
