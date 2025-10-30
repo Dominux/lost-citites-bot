3846a04becf7f1d9d490b80f2d9d10e73a51c703
Dominux <kir.gimranov@gmail.com>
2025-10-30 11:53:56 +0300

    Updated deps

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,4 +4,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+	optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
+	build: {
+		target: 'esnext'
+	},
 })
